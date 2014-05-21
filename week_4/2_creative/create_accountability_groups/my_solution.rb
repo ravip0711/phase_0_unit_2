bd0a896f899478047322fd1bc7f9458d847c3a8a
Ravi P <ravip0711@gmail.com>
2014-05-21 00:18:54 -0700

    updating week 4

--- week_4/2_creative/create_accountability_groups/my_solution.rb
+++ week_4/2_creative/create_accountability_groups/my_solution.rb
@@ -1,17 +1,39 @@
 # U2.W4: Create Accountability Groups
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Hethe Berg, we are actually from different cohort locations, I am in Chicago, and he is in San Francisco].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# Create a variable name
+# Value to array of names
+# Create 6 empty arrays for the names
+# Create the arrays so that they are random and separated into groups of 4
+# Due to odd number of people, last person will be in their own group
+#
 
 
 # 3. Initial Solution
 
+names = ["Ariel Fogel", "Hethe Berg", "Premila Anand", "Brian Junio", 
+	"John Akers", "Chandler Smith", "Gannon Curran", "Tony Leung", 
+	"Arik Gadye", "Allison Reilly", "Justin Harnoss", "Morgan O'Leary", 
+	"Puru Dahal", "Alex White", "Michele Murphy", "Matthew Darin", 
+	"Steven Leu", "Patrick Reynolds", "Lauren Kroner", 	"Albert Alquisola", 
+	"Ernie Salaza"]
+
+# Work in progress 
+def sorter(array)
+  remainder = []
+  x = []
+  x = array.shuffle.each_slice(4).to_a
+    if x.last.length <= 3 
+      remainder = x[-1]
+      x.pop
+    end
+    y =  x.zip(remainder)
+    y.each {|i| i.flatten!.compact!}
+end
 
 
 
