31ab8af32c0f5af5655b064b3d8a5128680446e9
Ravi P <ravip0711@gmail.com>
2014-06-11 00:44:50 -0700

    update for week 6

--- week_6/1_assert_statements/my_solution.rb
+++ week_6/1_assert_statements/my_solution.rb
@@ -1,31 +1,81 @@
 # U2.W6: Testing Assert Statements
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [By Myself].
 
 
 # 2. Review the simple assert statement
 
-def assert
-  raise "Assertion failed!" unless yield
-end
+# def assert
+#   raise "Assertion failed!" unless yield
+# end
 
-name = "bettysue"
-assert { name == "bettysue" }
-assert { name == "billybob" }
+# name = "bettysue"
+# assert { name == "bettysue" }
+# assert { name == "billybob" }
 
 # 2. Pseudocode what happens when the code above runs
+
+# The assert method raises an error when the yield is not correct. Yield is basically telling the code snippet to run the block which is at the bottom. Since a condition has been made that name = "bettysue", so the first block will run true making it pass, but the second block will not pass because it is false. It will then raise the error "Assetion failed".
 
 
 
 # 3. Copy your selected challenge here
 
+class Die
+  def initialize(sides)
+    if sides < 1
+        raise ArgumentError.new("Cannot have less than 1 side")
+    else
+        @sides = sides
+    end
+  end  
+  
+  def sides
+    @sides
+  end
+  
+  def roll
+    return rand(1..@sides)
+  end
+  
+end 
 
 
 
 # 4. Convert your driver test code from that challenge into Assert Statements
 
+def assert
+	raise "Assertion failed" unless yield
+end
+
+sides = 6
+assert {sides == 6}
+assert {sides == 5}
 
 
 
 
 # 5. Reflection
+
+# What parts of your strategy worked? What problems did you face?
+# I think this activity was pretty easy with the assert method. I wasn't sure if the code would recognize sides or @sides. Didn't really know which one to pick, just tried it out.
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+# I had questions on how to make the assert driver test code more complex. I did the basic check on making sure if the dice I picked and gave it 6 sides, that I would return back 6 if I called the method for sides. 
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+#I haven't been able to understand why we need to use assert methods for driver test code. Why can't we just continue to use for example, sides == 6? I was also trying to make an assert driver test for the roll method but I couldn't figure out how to make it work.
+
+# Did you learn any new skills or tricks?
+#I just learned how to use the assert method as a driver test code. Not sure exactly how it will help compared to the regular driver test code used.
+
+# How confident are you with each of the Learning Competencies?
+#I am pretty confident with the learning competencies atleast for simple assert methods.
+
+# Which parts of the challenge did you enjoy?
+#I enjoyed creating the assert method and checking if it works. I used trial and error method to make sure what will work.
+
+# Which parts of the challenge did you find tedious?
+#I found it tedious that I knew what I wanted to test, but I couldn't figure out how to do it. For example, I wanted to create an assert test for my roll method, but I couldn't figure out how to do it.
+
+
--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,71 +1,80 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [By myself].
 
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+  attr_reader :contents
 
-# Are there any more methods needed in this class?
+# Are there any more methods needed in this class? #not sure
 
-def initialize
-@contents = []
-@open = true
-end
+  def initialize
+    @contents = []
+    @open = true
+  end
 
-def open
-@open = true
-end
+  def open
+    @open = true
+  end
 
-def close
-@open = false
-end 
+  def close
+    @open = false
+  end 
 
-def add_item
-@contents << item
-end
+  def add_item(item) #used to push items into the contents array
+    @contents << item
+  end
 
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
+  def remove_item(item = @contents.pop) #what is `#pop` doing? Pop is removing the item. I'm not sure if its removing the item used in the parameter or just the last item in the @contents.
+    @contents.delete(item)
+  end
 
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
+  def dump  # what should this method return? This method should remove all the items in the drawer.
+    @contents.clear # I added this method to remove all the elements in the @contents array
+    puts "Your drawer is empty."
+  end
 
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
+  def view_contents
+    puts "The drawer contains:"
+    @contents.each {|silverware| puts "- " + silverware.type }
+  end
 end
 
 
 class Silverware
-attr_reader :type
+  attr_reader :type
 
-# Are there any more methods needed in this class?
+# Are there any more methods needed in this class? #not sure
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
+  def initialize(type, clean = true)
+    @type = type
+    @clean = clean
+  end
+
+  def eat
+    puts "eating with the #{type}"
+    @clean = false
+  end
+
+  def clean_silverware
+    "needs to be cleaned" #was not sure how to put the item that is called on the .eat method in this return statement. 
+  end
+
+  def dishwasher #added this method to put items that are not clean
+    "#{type} needs to be put in the dishwasher"
+  end
 end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
-end
-
-knife1 = Silverware.new("knife")
+knife1 = Silverware.new("knife")#1 way to add knife
 
 silverware_drawer = Drawer.new
-silverware_drawer.add_item(knife1) 
-silverware_drawer.add_item(Silverware.new("spoon"))
-silverware_drawer.add_item(Silverware.new("fork")) 
+silverware_drawer.add_item(knife1) #1 way to add knife
+silverware_drawer.add_item(Silverware.new("spoon"))#second way to add items
+silverware_drawer.add_item(Silverware.new("fork")) #second way to add items
 silverware_drawer.view_contents
 
 silverware_drawer.remove_item
@@ -83,20 +92,51 @@ removed_knife.clean_silverware
 
 silverware_drawer.view_contents
 silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
+silverware_drawer.view_contents #What should this return? This should return nothing because we used the dump method to dump all the contents of the drawer.
 
-
+fork = Silverware.new("fork") #set fork as a new object inside silverware
+silverware_drawer.add_item(fork) #set fork as a new item added to the silverware_drawer
 fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
 fork.eat
+silverware_drawer.view_contents
 
 #BONUS SECTION
-# puts fork.clean
+#puts fork.clean
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+def assert
+  raise "Assertion failed" unless yield
+end
 
-
-
+silverware_drawer = Drawer.new
+silverware_drawer.add_item(Silverware.new("spork"))
+assert {silverware_drawer == ["spork"]} #Had trouble getting this to work. I wanted to test to see if "spork" has been added to the array. How can we test to see if we got an empty array?
 
 
 # 5. Reflection 
+# What parts of your strategy worked? What problems did you face?
+#I first tried to read through everything to get a big picture on what this code was about. I understood some elements and some I couldn't follow through. I then decided to break up the code and write notes on what each method is doing so I can refer back to it easily. I faced the problem of forgetting what attached to what. The names start mixing up when your using the same keywords, so I had problems with keeping track of the variable names mixed with method names.
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+#I had a question about a method that uses a parameter and that parameter is assigned to another variable with a method. It was this line that I could not understand: def remove_item(item = @contents.pop). I was not sure if it was an error or the syntax was right by my understanding of it was wrong. I also had questions about how to create driver tests where I can see if an array returns empty.
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+#I am still having trouble with driver tests. I do understand the assert driver tests, but get stuck on how to write my own and impliment them.
+
+# Did you learn any new skills or tricks?
+#I did learn a new method called .clear. This will clear all the elements within an array. I used it for the dump method in this code.
+
+# How confident are you with each of the Learning Competencies?
+# I am pretty confident with the learning competencies. I fixed majority of the errors, but just wasn't sure if it was the right way. I used some trial and error ways to check if the error cleared. I do understand how to look for errors and find clues in them that will help me clear the error.
+
+# Which parts of the challenge did you enjoy?
+#I did enjoy going through the code and learning about how everything falls together. I now know how a ruby code can be used. I like to learn from examples and this example was a pretty good translation to something in real life. This example was about having silverware in a drawer and keeping track of what goes in and out of the drawer. I actually enjoyed fixing the errors.
+
+
+# Which parts of the challenge did you find tedious?
+#The tedious part was going through the fairly long code (atleast the code I have seen so far) and trying to make sense of it all. There were some code elements that did not make sense to me. I also found it tedious that if I cleared some of the errors, the errors I could not clear I would just have to resort to trial and error. I tested the code until it worked.
+
+
+
+
